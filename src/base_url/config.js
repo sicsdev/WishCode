f3730695232c35efb0d8bd248ee86076639fe61d
Yash Kumar <sicsdev21@gmail.com>
2022-04-08 02:31:14 +0530

    path change

--- src/base_url/config.js
+++ src/base_url/config.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
-    // baseURL: 'https://sicsdev.com/wishtrax/api',
+    // baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://sicsdev.com/wishtrax/api',
 });
-    export const imageBaseUrl = 'http://127.0.0.1:8000';
-//  export const imageBaseUrl = 'https://sicsdev.com/wishtrax';
+    // export const imageBaseUrl = 'http://127.0.0.1:8000';
+ export const imageBaseUrl = 'https://sicsdev.com/wishtrax';
 export default axiosConfig;
