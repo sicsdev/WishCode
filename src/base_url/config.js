25252dc42604919a8e0d5d02ab949cb785088920
Yash Kumar <sicsdev21@gmail.com>
2022-04-12 03:00:33 +0530

    config

--- src/base_url/config.js
+++ src/base_url/config.js
@@ -5,5 +5,5 @@ const axiosConfig = axios.create({
     baseURL: 'http://137.184.128.65/api',
 });
     // export const imageBaseUrl = 'http://127.0.0.1:8000';
- export const imageBaseUrl = 'http://137.184.128.65/wishtrax';
+ export const imageBaseUrl = 'http://137.184.128.65';
 export default axiosConfig;
