b34002cc5dfd80eb50f9e4646d1141e8d448e598
Yash Kumar <sicsdev21@gmail.com>
2022-04-13 04:34:28 +0530

    Delete back-end

--- backend/resources/views/emails/send-company-request-status-mail.blade.php
+++
@@ -1,8 +0,0 @@
-@component('mail::message')
-<h2>Dear {{ $data['name'] }}</h2>
-
-<p>Your Company Request {{ $data['status'] }} successfully!</p>
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
