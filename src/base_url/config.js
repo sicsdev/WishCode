a5c0ca4d1ea12efdd598800b16cfae61e749b07a
sicsdev <sics21@gmail.com>
2024-01-31 13:06:26 +0000

    chagnes in baseurl

--- src/base_url/config.js
+++ src/base_url/config.js
@@ -5,10 +5,5 @@ const axiosConfig = axios.create({
     baseURL: 'https://demo.wishtrax.com/api',
 });
     // export const imageBaseUrl = 'http://127.0.0.1:8000';
-<<<<<<< HEAD
- export const imageBaseUrl = 'http://137.184.128.65';
-export default axiosConfig;
-=======
  export const imageBaseUrl = 'https://demo.wishtrax.com/';
 export default axiosConfig;
->>>>>>> 0232d2f94364c1142d80f007c3037c0efba9ff4f
