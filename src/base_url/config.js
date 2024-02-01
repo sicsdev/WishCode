e38aa713cd5cfb9ac8bdb9b3bff54d05762f63f4
sics <sicsdev21@gmail.com>
2024-02-01 15:50:03 +0530

    changes in base route

--- src/base_url/config.js
+++ src/base_url/config.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosConfig = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api',
-    baseURL: 'https://demo.wishtrax.com/api',
+    baseURL: 'http://dev1.wishtrax.com/api',
 });
     // export const imageBaseUrl = 'http://127.0.0.1:8000';
- export const imageBaseUrl = 'https://demo.wishtrax.com/';
+ export const imageBaseUrl = 'http://dev1.wishtrax.com';
 export default axiosConfig;
