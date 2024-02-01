cdfae86b06833b48995543c0c951f29ca7a99a3b
sicsdev <84189073+sicsdev@users.noreply.github.com>
2024-02-01 16:02:41 +0530

    Update config.js

--- src/base_url/config.js
+++ src/base_url/config.js
@@ -5,10 +5,6 @@ const axiosConfig = axios.create({
     baseURL: 'http://dev1.wishtrax.com/api',
 });
     // export const imageBaseUrl = 'http://127.0.0.1:8000';
-<<<<<<< HEAD
- export const imageBaseUrl = 'https://demo.wishtrax.com/';
-export default axiosConfig;
-=======
  export const imageBaseUrl = 'http://dev1.wishtrax.com';
 export default axiosConfig;
->>>>>>> e38aa713cd5cfb9ac8bdb9b3bff54d05762f63f4
+
