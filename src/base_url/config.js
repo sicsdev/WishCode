e02544a59898abd466f08283f5a4a59a8fe3c1a2
sicsdev <sics21@gmail.com>
2024-02-05 11:15:30 +0000

    chagnes in config file

--- src/base_url/config.js
+++ src/base_url/config.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosConfig = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api',
-    baseURL: 'https://dev1.wishtrax.com/api',
+    baseURL: 'https://api.wishtrax.com/api/',
 });
     // export const imageBaseUrl = 'http://127.0.0.1:8000';
- export const imageBaseUrl = 'https://dev1.wishtrax.com';
+ export const imageBaseUrl = 'https://api.wishtrax.com';
 export default axiosConfig;
 
